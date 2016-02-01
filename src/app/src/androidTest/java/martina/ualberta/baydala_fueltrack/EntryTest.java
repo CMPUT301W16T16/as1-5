77dca1cc23409326bdb25932b3c4febcca1da5ab
mbaydala <baydala@ualberta.ca>
2016-02-01 13:16:42 -0700

    fixed some tests and updated APK

--- src/app/src/androidTest/java/martina/ualberta/baydala_fueltrack/EntryTest.java
+++ src/app/src/androidTest/java/martina/ualberta/baydala_fueltrack/EntryTest.java
@@ -12,35 +12,33 @@ public class EntryTest extends ActivityInstrumentationTestCase2 {
 
     public void testGetFuelAmount() {
         Entry entry = new Entry();
-        entry.setFuelAmount("50");
-        assertEquals(50.0, Double.valueOf(entry.getFuelAmount()));
+        entry.setFuelAmount(50);
+        assertEquals(50.0, entry.getFuelAmount());
     }
 
     public void testGetUnitCost() {
         Entry entry = new Entry();
-        entry.setUnitCost("50");
-        assertEquals(50.0, Double.valueOf(entry.getUnitCost()));
+        entry.setUnitCost(50);
+        assertEquals(50.0, entry.getUnitCost());
     }
 
     public void testGetFuelCost() {
         Entry entry = new Entry();
-        entry.setFuelAmount("50");
-        entry.setUnitCost("70.6");
+        entry.setFuelAmount(50);
+        entry.setUnitCost(70.6);
         double expected_fuel_cost = 50 * 70.6 / 100.0;
         assertEquals(entry.getFuelCost(), expected_fuel_cost);
 
-        entry.setFuelAmount("0.0");
-        entry.setUnitCost("0.0");
+        entry.setFuelAmount(0.0);
+        entry.setUnitCost(0.0);
         expected_fuel_cost = 0 * 0 / 100.0;
         assertEquals(entry.getFuelCost(), expected_fuel_cost);
     }
 
     public void testToString() {
-                        // Date         Odometer Station Grade Amount Cost/L  Entry Number
-        String[] values = {"2016-01-01", "1000", "Husky", "87", "60", "70.5", "5"};
-        Entry entry = new Entry();
+        Entry entry = new Entry(5, "2016-01-01", "Husky", 1000, "87", 60, 70.5);
 
-        assertEquals(entry.toString(), "Entry 5 | 2016-01-01");
+        assertEquals(entry.toString(), "Entry 5 | 2016-01-01 | $42.30");
     }
 }
 
